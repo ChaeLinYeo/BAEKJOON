eef417341370658d92c8972d98b03a2c99be5c22
ChaeLinYeo <evergreen786@naver.com>
2020-08-12 00:38:51 +0900

    10171

--- 10171.c
+++ 10171.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(void){
-    printf("\    /\\\n");
+    printf("\\    /\\\n");
     printf(" )  ( ')\n");
     printf("(  /  )\n");
-    printf(" \(__)|");
+    printf(" \\(__)|");
 }
+// \를 출력하기 위해서는 두번 써줘야 한다.
+// 각 문단을 엔터로 출력하기 위해서는 \n을 써줘야 한다.
