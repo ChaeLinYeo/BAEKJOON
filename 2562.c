111874a6e626f1a2248d441cf79ff7aa95edfcb9
ChaeLinYeo <evergreen786@naver.com>
2020-08-17 16:33:20 +0900

    Update 2562.c

--- 2562.c
+++ 2562.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
-    int num = 0;
-    int pos;
-    int max = num;
-    for(int i=1; i<=9; i++){
-        scanf("%d", &num);
-        if(num>max){
-            max = num;
-            pos = i;
+    int num = 0;    //입력받을 9개의 값
+    int pos;    //값의 위치
+    int max = num;  //최댓값을 일단 num과 일치시켜둔다.
+    for(int i=1; i<=9; i++){    //9번 비교
+        scanf("%d", &num);  //9번 값을 입력받는다.
+        if(num>max){    //입력받은 값이 기존의 max=num값보다 크면
+            max = num;  //max 값을 바꿔준다.
+            pos = i;    //max에 들어간 현재 값의 위치를 저장한다.
         }
     }
-    printf("%d\n%d", max, pos);
+    printf("%d\n%d", max, pos); //최댓값과 그 값의 순서 출력
 }
