55e3a7b87a60c89f6034b204ca0063a7023a4461
ChaeLinYeo <evergreen786@naver.com>
2020-08-18 17:25:15 +0900

    edit 10828.c

--- 10828.c
+++ 10828.c
@@ -7,6 +7,13 @@ int main(){
     int pushnum;    //스택에 push할 정수
     int top = 0;    //스택의 맨 위
 
+//예시
+//quest     top     stack
+//          0       stack[0] = 0
+//push 1    1       stack[0] = 1
+//push 2    2       stack[1] = 2
+//pop       1       
+
     scanf("%d", &N);    //명령문 개수를 입력받는다.
     for(int i=0; i<N; i++){
         scanf("%s", quest);
@@ -20,7 +27,10 @@ int main(){
             if(top == 0){
                 printf("%d\n", -1);
             }else{
-                printf("%d\n", stack[top-1]);
+                printf("%d\n", stack[top-1]);   
+                //pop되었을때 증가한 top값에서 1을 빼야 한다.
+                //예시를 보면 알겠지만, pop될 정수를 출력하기 위해서는 top-1자리에 있는 것을 출력해야 한다.
+                //안그러면 0이 출력됨!!
                 top--;
             }
         }
@@ -41,7 +51,7 @@ int main(){
             if(top == 0){
                 printf("%d\n", -1);
             }else{
-                printf("%d\n", stack[top-1]);
+                printf("%d\n", stack[top-1]);   //위의 설명과 동일
             }
         }
     }
