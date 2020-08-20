46eca1ea0e5add88010574d5e5e5aaeb8ecfbaea
ChaeLinYeo <evergreen786@naver.com>
2020-08-20 20:15:53 +0900

    5089.c

--- 5086.c
+++ 5086.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int main(void){
-    int T, N, num;
-    int sum = 0;
-    scanf("%d", &T);    //테스트 케이스 입력
-    for(int i=0; i<T; i++){
-        scanf("%d", &N);    //자연수의 개수 입력
-        for(int i=0; i<N; i++){
-            scanf("%d", &num);
-            sum += num;
+    int n1, n2;
+    while(1){
+        scanf("%d %d", &n1, &n2);
+        if(n1 == 0 && n2 == 0){
+            break;
         }
-        printf("%d\n", sum);
-        sum = 0;
+        else if(n2%n1 == 0){ //첫 번째 숫자가 두 번째 숫자의 약수라면
+            printf("factor\n");
+        }else if(n1%n2 == 0){   //첫 번째 숫자가 두 번째 숫자의 배수라면
+            printf("multiple\n");
+        }else{  //둘 다 아니라면 
+            printf("neither\n");
+        }
     }
-
+    return 0;
 }
