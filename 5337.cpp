71cc3308416183639b6699520a40933c63c08f51
ChaeLinYeo <evergreen786@naver.com>
2020-08-24 16:42:29 +0900

    edit 5337.cpp

---
+++ 5337.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    cout << ".  .   ."<<"\n";
+    cout << "|  | _ | _. _ ._ _  _" << "\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << "\n";  // '\'을 출력하려면 '\\'이렇게 두번 입력해야 한다.
+    return 0;
+}
