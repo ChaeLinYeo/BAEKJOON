2950ea432791eab95e12f77246b86100a6030748
ChaeLinYeo <evergreen786@naver.com>
2020-09-06 14:29:14 +0900

    Update 4101.py

--- 4101.py
+++ 4101.py
@@ -1,8 +1,8 @@
-T = int(input())
-for _ in range(T):  #변수 _가 T값을 가지고 반복을 수행한다. 실제로 쓰이는 변수가 아니므로 _로 처리
-    num = list(map(int, input().split()))
-    even_num = []
-    for i in num:
-        if(i%2 == 0):
-            even_num.append(i)
-    print(sum(even_num), min(even_num))
+# 두 양의 정수가 주어졌을 때, 첫 번째 수가 두 번째 수보다 큰지 구하는 프로그램
+while True:
+    A, B = input().split()
+    A = int(A)
+    B = int(B)
+    if A == 0 and B == 0 : break
+    if A <= B : print("No")
+    else : print("Yes")
