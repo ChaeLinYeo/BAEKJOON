ae954c432d25bfb2c91ccd16e4556b5b6f9c92f0
ChaeLinYeo <evergreen786@naver.com>
2020-09-06 11:47:59 +0900

    Update 3034.py

--- 3034.py
+++ 3034.py
@@ -1,14 +1,9 @@
-location = [list(map(int, input().split())) for _ in range(3)]
-x1 = location[0][0]
-y1 = location[0][1]
-x2 = location[1][0]
-y2 = location[1][1]
-x3 = location[2][0]
-y3 = location[2][1]
-if(x1 == x2) : x = x3
-elif(x2 == x3) : x = x1
-elif(x1 == x3) : x = x2
-if(y1 == y2) : y = y3
-elif(y2 == y3) : y = y1
-elif(y1 == y3) : y = y2
-print(x,y)
+N, W, H = input().split()
+N = int(N)
+W = int(W)
+H = int(H)
+diagonal = (W**2+H**2)**0.5
+for i in range(N):
+    fire = int(input())
+    if(fire <= diagonal) : print("DA")
+    else : print("NE")
