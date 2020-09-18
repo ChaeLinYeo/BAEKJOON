d30f589b79af2be3c536717127c07eeccf8f492f
ChaeLinYeo <evergreen786@naver.com>
2020-09-18 10:32:03 +0900

    10101

---
+++ 10101.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = int(input())
+C = int(input())
+if(A == B == C == 60):print("Equilateral")
+elif(A+B+C == 180 and (A==B or B==C or A==C)):print("Isosceles")
+elif(A+B+C == 180 and (A!=B and B!=C and A!=C)):print("Scalene")
+elif(A+B+C != 180):print("Error")
