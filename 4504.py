089be913418fef0e736277771f703c07bb38ac7d
ChaeLinYeo <evergreen786@naver.com>
2020-09-18 11:18:00 +0900

    solve 4504

---
+++ 4504.py
@@ -0,0 +1,6 @@
+n = int(input())
+while(True):
+    num = int(input())
+    if(num == 0):break
+    elif(num%n == 0):print(num," is a multiple of ",n,".",sep='')
+    else:print(num," is NOT a multiple of ",n,".",sep='')
