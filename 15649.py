b165d3a09e27ee49f2c367a28df213f8b33ae3a7
ChaeLinYeo <evergreen786@naver.com>
2020-09-21 14:49:26 +0900

    solve

--- 15649.py
+++ 15649.py
@@ -14,12 +14,9 @@ def DFS(x):
     for i in range(n):
         if check_number[i]:               # 중복될 경우 패스
             continue
-
         array.append(number_list[i])      # 수열 추가
         check_number[i] = True            # 사용한 수 체크
-
         DFS(x + 1)                        # + 1번째 수열을 위해 재귀함수 호출
-
         array.pop()                       # 수열 마지막 자리 삭제
         check_number[i] = False           # 사용한 수 초기화
 DFS(0)
